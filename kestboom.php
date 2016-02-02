44ca75501bc16248d46571e46010c06d670c8a11
romario dindajal <romario dindajal>
2016-02-02 13:37:37 +0100

    First commit

---
+++ kestboom.php
@@ -0,0 +1,12 @@
+<?php
+
+$jaar = 6;
+
+//Teken het aantal kaarsjes op de taart...
+$kaarsjes = 0;
+while ($kaarsjes < $jaar) { // Zolang $kaarsjes kleiner is dan 6 ...
+	echo " TESTEN"; // ….teken je kaarsjes.
+	$kaarsjes++; // is hetzelfde als $kaarsjes = $kaarsjes + 1
+}
+?>
+
